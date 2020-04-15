d339f6e825f346e165edc9f1157c70b42f245196
Chase Hooper <Hoopchaser@gmail.com>
2020-04-15 15:20:25 -0600

    mvp done and added a way to find a single social path

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,6 @@
+import random
+from util import Queue
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -32,10 +35,8 @@ class SocialGraph:
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
@@ -43,10 +44,39 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
         # Add users
+        # Use add_user num_users times
 
-        # Create friendships
+        # Create friendships based on number of users
+        for i in range(0, num_users):
+            self.add_user(f"User {i+1}")
+
+        # Generate all friendship combinations
+        possible_friendships =  []
+
+        # Avoid dupes by making sure first number is smaller than second
+        #For each created user
+        for user_id in self.users:
+            for friend_id in range(user_id+1, self.last_id+1):
+                possible_friendships.append((user_id, friend_id))
+
+        # Shuffle all possible friendships
+        random.shuffle(possible_friendships)
+
+        # Create for first X pairs x is total //2
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+
+        # * Hint 1: To create N random friendships, you could create a
+        # list with all possible friendship combinations, shuffle the
+        # list, then grab the first N elements from the list. You will
+        # need to `import random` to get shuffle.
+        # * Hint 2: `add_friendship(1, 2)` is the same as
+        # `add_friendship(2, 1)`. You should avoid calling one after
+        # the other since it will do nothing but print a warning. You
+        # can avoid this by only creating friendships where user1 < user2.
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,14 +87,69 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        def get_neighbors(node_id):
+            neighbors = []
+
+            for friend in self.friendships[node_id]:
+                neighbors.append(friend)
+
+            return neighbors
+
+        queue = Queue()
+        queue.enqueue([user_id])
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+
+        #BFS
+        while queue.size() > 0:
+            current_path = queue.dequeue()
+
+            current_node = current_path[-1]
+
+            if current_node not in visited:
+                visited[current_node] = current_path
+
+                for neighbor in get_neighbors(current_node):
+                    queue.enqueue([*current_path, neighbor])
+
         return visited
+
+    def get_one_social_path(self, user_id, target_id):
+
+        #Returns all the friends of a node in an array
+        def get_neighbors(node_id):
+            neighbors = []
+            for friend in self.friendships[node_id]:
+                neighbors.append(friend)
+            return neighbors
+
+        #Create a queue with the user ID in queue
+        queue = Queue()
+        queue.enqueue([user_id])
+        visited = {}  # Note that this is a dictionary, not a set
+
+        #BFS
+        while queue.size() > 0:
+            current_path = queue.dequeue()
+
+            current_node = current_path[-1]
+
+            if current_node not in visited:
+                visited[current_node] = current_path
+
+                if current_node == target_id:
+                    return current_path
+
+                for neighbor in get_neighbors(current_node):
+                    queue.enqueue([*current_path, neighbor])
+
+        return None
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph(20, 4)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
-    print(connections)
+    #print(connections)
+    connection = sg.get_one_social_path(1, 5)
+    print(connection)
